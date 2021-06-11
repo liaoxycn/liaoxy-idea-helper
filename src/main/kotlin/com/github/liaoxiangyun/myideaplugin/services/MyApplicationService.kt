efd82e2741f4742df6be036e577b4651af75fcf0
GitHub Action <action@github.com>
2021-06-11 09:34:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/liaoxiangyun/myideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/liaoxiangyun/myideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.liaoxiangyun.myideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.liaoxiangyun.myideaplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/liaoxiangyun/myideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/liaoxiangyun/myideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.liaoxiangyun.myideaplugin.services
 
+import com.github.liaoxiangyun.myideaplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
